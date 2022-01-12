9295d15fcd0f01b0be127d4fa6a878521eada7ab
milan <milanmshah04@gmail.com>
2022-01-12 14:44:17 -0500

    fixing errors

--- frc2022-Imported/src/main/java/frc/robot/commands/ArcadeDrive.java
+++ frc2022-Imported/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -5,31 +5,43 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.Drive;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.kauailabs.navx.frc.AHRS;
 
 /** An example command that uses an example subsystem. */
 public class ArcadeDrive extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final Drive m_subsystem;
+  private final Drive drive;
+  private AHRS navx;
+  private Joystick j;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ArcadeDrive(Drive subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+  public ArcadeDrive(Drive drive, Joystick j) {
+    this.drive = drive;
+    this.j = j;
+    navx = drive.getNavxInstance(); 
+    addRequirements(drive);
+
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    navx.zeroYaw();
+    drive.resetEncoders();
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    drive.arcadeDrive(j.getY(), -1*j.getX());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
