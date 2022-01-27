b0dffb3bd0db7055fe6594c9166da09438804283
milan <milanmshah04@gmail.com>
2022-01-27 13:21:07 -0500

    update again

--- test/src/main/java/frc/robot/Robot.java
+++ test/src/main/java/frc/robot/Robot.java
@@ -7,37 +7,26 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-/**
- * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
- * arcade steering.
- */
 public class Robot extends TimedRobot {
-  private final WPI_TalonSRX m_leftMotor = new WPI_TalonSRX(1);
-  private final WPI_TalonSRX m_rightMotor = new WPI_TalonSRX(2);
-  private final WPI_TalonSRX m_rightMotor1 = new WPI_TalonSRX(3);
-  private final WPI_TalonSRX m_leftMotor1 = new WPI_TalonSRX(4);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
-  private final Joystick m_stick = new Joystick(0);
+  private final WPI_TalonSRX m1 = new WPI_TalonSRX(1);
+  private final WPI_TalonSRX m2 = new WPI_TalonSRX(2);
+  private final WPI_TalonSRX m3 = new WPI_TalonSRX(3);
+  private final WPI_TalonSRX m4 = new WPI_TalonSRX(4);
 
   @Override
   public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor1.follow(m_rightMotor);
-    m_leftMotor1.follow(m_leftMotor);
-    m_rightMotor.setInverted(true);
-    m_rightMotor1.setInverted(true);
   }
 
   @Override
   public void teleopPeriodic() {
-    // Drive with arcade drive.
-    // That means that the Y axis drives forward
-    // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(-m_stick.getY(), m_stick.getX());
+    SmartDashboard.putString("called", "abc");
+    m1.set(1);
+    m2.set(1);
+    m3.set(1);
+    m4.set(1);
   }
 }
